2706caa4f3f22295b9b0bde7d82d8ccf2b2221b4
Austin Welsh-Graham <admin@toastysoftware.co.uk>
2026-08-02 08:36:12 +0100

    Update(field_struct_types): Add comments.

--- src/field_struct_types.cc
+++ src/field_struct_types.cc
@@ -2,20 +2,30 @@
 
 namespace muehle {
 
+/*
+ * Returns a reference to the current player
+ */
 const PlayerStruct::Core& FieldStructTypes::Core::GetCurPlayer() const {
   return cur_player;
 }
 
+/*
+ * Returns a reference to the opponent player
+ */
 const PlayerStruct::Core& FieldStructTypes::Core::GetOppPlayer() const {
   return opp_player;
 }
 
+/*
+ * Returns the player id of a stone
+ */
 PlayerId FieldStructTypes::Core::GetStone(FieldPos pos) const {
   return field[pos];
 }
 
-bool FieldStructTypes::Core::InSettingPhase() const {
-  return setting_phase;
-}
+/*
+ * Returns true if the game is in the setting phase
+ */
+bool FieldStructTypes::Core::InSettingPhase() const { return setting_phase; }
 
 }  // namespace muehle
