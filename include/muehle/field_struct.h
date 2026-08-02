586b97db32debae660bc6b847310a608585523bc
Austin Welsh-Graham <admin@toastysoftware.co.uk>
2026-08-02 12:48:00 +0100

    Add(field_struct and field_struct_reverse): Add FieldStruct and FieldStructReverse headers. Still need to do the full implementation of them both.

--- include/muehle/field_struct.h
+++ include/muehle/field_struct.h
@@ -1,10 +1,28 @@
 #ifndef MUEHLE_FIELD_STRUCT_H_
 #define MUEHLE_FIELD_STRUCT_H_
 
+#include "muehle/field_struct_forward.h"
+#include "muehle/field_struct_reverse.h"
+
 namespace muehle {
 
-// Declaration goes here.
+/* Class representing the field. This master class is supposed to be used by the
+ * class consumers. */
+class FieldStruct : public FieldStructForward, public FieldStructReverse {
+  friend class StateAddressing;
 
+ public:
+  /* Constructor */
+  FieldStruct();
+  FieldStruct(const FieldStruct& other);
+  ~FieldStruct();
+
+  void GetPredecessors_2(
+      std::vector<FieldStructTypes::Core>& pred_fields) const;
+
+  /* Operators */
+  bool operator==(const FieldStruct& other) const;
+};
 }  // namespace muehle
 
 #endif  // MUEHLE_FIELD_STRUCT_H_
--- include/muehle/field_struct_reverse.h
+++ include/muehle/field_struct_reverse.h
@@ -1,10 +1,37 @@
 #ifndef MUEHLE_FIELD_STRUCT_REVERSE_H_
 #define MUEHLE_FIELD_STRUCT_REVERSE_H_
 
+#include <vector>
+
+#include "muehle/field_struct_variables.h"
+
 namespace muehle {
 
-// Declaration goes here.
+/* Functions related to get the predecessor states */
+class FieldStructReverse : virtual public FieldStructVariables {
+ public:
+  void GetPredecessors(std::vector<FieldStructTypes::Core>& pred_fields) const;
 
+ private:
+  /* Get predecessors */
+  void GetPredecessors_normalMove(
+      std::vector<FieldStructTypes::Core>& pred_fields,
+      FieldStructReverse& field, bool mill_was_closed) const;
+  void GetPredecessors_jumpingPhase(
+      std::vector<FieldStructTypes::Core>& pred_fields,
+      FieldStructReverse& field, bool mill_was_closed) const;
+  void GetPredecessors_settingPhase(
+      std::vector<FieldStructTypes::Core>& pred_fields,
+      FieldStructReverse& field, bool mill_was_closed) const;
+  void GetPredecessors_stoneRemove(
+      std::vector<FieldStructTypes::Core>& pred_fields,
+      FieldStructReverse& field) const;
+
+  bool StorePredecessor(std::vector<FieldStructTypes::Core>& pred_fields,
+                        const FieldStructReverse& field) const;
+  bool AnyLonelyStone(const FieldStructReverse& field,
+                      FieldPos removed_from) const;
+};
 }  // namespace muehle
 
 #endif  // MUEHLE_FIELD_STRUCT_REVERSE_H_
