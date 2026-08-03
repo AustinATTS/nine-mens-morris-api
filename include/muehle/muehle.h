2018fb25ddaf27de3095f3c36f4e52d4daff6040
Austin Welsh-Graham <admin@toastysoftware.co.uk>
2026-08-03 06:48:00 +0100

    Add(muehle): Add the header file for the Muehle class and a little bit of its implementation. Still need to finish it off.

--- include/muehle/muehle.h
+++ include/muehle/muehle.h
@@ -1,9 +1,115 @@
 #ifndef MUEHLE_MUEHLE_H_
 #define MUEHLE_MUEHLE_H_
 
+#include <cstdio>
+#include <cstdlib>
+
+#include "muehle/field_struct.h"
+#include "muehle/muehle_ai.h"
+
 namespace muehle {
 
-// Declaration goes here.
+/* Class representing the game */
+class Muehle {
+  /* typedef */
+  using FieldPos = FieldStruct::FieldPos;
+  using FieldArray = FieldStruct::FieldArray;
+
+ public:
+  struct LogItem {
+    MoveInfo move = MoveInfo{};                 /* Move which is done */
+    PlayerId player = PlayerId::square_is_free; /* Player who made the move */
+
+    LogItem() = default;
+    LogItem(const MoveInfo& move, PlayerId player);
+
+    static unsigned int GetNumNormalMovesWithoutRemoveal(
+        const std::vector<LogItem>& log);
+    static float GetNumRepeatedMoves(const std::vector<LogItem>& log);
+    bool operator==(const LogItem& other) const;
+  };
+
+  /* Constants */
+  static const unsigned int max_num_moves =
+      10000; /* Maximum number of moves which can be saved in the history */
+  static const unsigned int num_repeated_moves_to_remis =
+      3; /* Number of repeated moves after which the game is remis */
+
+  /* Constructor / destructor */
+  Muehle();
+  ~Muehle();
+
+  /* Functions */
+  void BeginNewGame(MuehleAI* first_player_ai, MuehleAI* second_player_ai,
+                    PlayerId current_player, bool setting_phase,
+                    bool reset_field);
+  void SetAI(PlayerId player, MuehleAI* ai);
+  bool MoveStone(const MoveInfo& move);
+  void SetNumMovesToRemis(unsigned int num_moves);
+  bool RedoLastMove();
+  bool UndoLastMove();
+
+  /* Printing */
+  void PrintField() const;
+
+  /* Start the game with a customised state */
+  bool SetCurrentGameState(FieldStruct& cur_state);
+  bool PutStone(FieldPos pos, PlayerId player);
+  bool SettingPhaseHasFinished();
+  void CalcNumberOfRestingStones(int& num_white_stones_resting,
+                                 int& num_black_stones_resting);
+
+  /* Get computer choice */
+  void GetComputersChoice(MoveInfo& move) const;
+  void GetChoiceOfSpecialAI(MuehleAI* ai, MoveInfo& move) const;
+
+  /* Getter */
+  void GetLog(std::vector<LogItem>& log, unsigned int& current_index) const;
+  const FieldArray& GetField() const;
+  bool WouldMillBeClosed(const MoveInfo& move) const;
+  bool IsCurrentPlayerHuman() const;
+  bool IsOpponentPlayerHuman() const;
+  bool InSettingPhase() const { return field.InSettingPhase(); }
+  unsigned int MustStoneBeRemoved() const { return stone_must_be_removed; }
+  bool GameHasFinished() const;
+  PlayerId GetWinner() const;
+  PlayerId GetCurrentPlayer() const { return field.GetCurPlayer().id; }
+  unsigned int GetMovesDone() const { return move_log_current_index; }
+  unsigned int GetNumStonesSet() const { return field.GetNumStonesSet(); }
+  PlayerId GetBeginningPlayer() const { return beginning_player; }
+  unsigned int GetNumStonesOfCurPlayer() const {
+    return field.GetCurPlayer().num_stones;
+  }
+  unsigned int GetNumStonesOfOppPlayer() const {
+    return field.GetOppPlayer().num_stones;
+  }
+  unsigned int GetNumTurnsToRemis() const;
+  float GetNumRepeatedMoves() const;
+  bool IsMoveAllowed(const MoveInfo& move, bool IgnoreStoneRemoval) const;
+
+ private:
+  /* Variables */
+  bool stone_must_be_removed =
+      false; /* True if a mill was closed and the player must remove a stone.
+                This also indicates that the move is not completed yet. */
+  unsigned int move_log_current_index =
+      0; /* Index pointing to the current move in the history, when the user
+            already went back on some moves */
+  unsigned int num_moves_to_remis = 205; /* Number of moves after which the game
+              is remis. This is not the current but the initial value. */
+  std::vector<LogItem>
+      move_log; /* Array containing the history of moves done. */
+  MuehleAI* player_one_ai = nullptr; /* Class pointer to the AI of player one */
+  MuehleAI* player_two_ai = nullptr; /* Class pointer to the AI of player two */
+  FieldStruct field;                 /* Current field */
+  FieldStruct initial_field; /* Undo of the last move is done by setting the
+                                initial field and performing all moves saved in
+                                history. the initial field is not necessarily an
+                                empty field. It can be any state. */
+  PlayerId beginning_player =
+      PlayerId::player_one; /* PlayerId of the player who makes the first move
+                             */
+};
 
 }  // namespace muehle
 
--- src/muehle.cc
+++ src/muehle.cc
@@ -1,7 +1,81 @@
 #include "muehle/muehle.h"
 
+#include <c++/13/ctime>
+
 namespace muehle {
 
-// Implementation goes here.
+/* Muehle class constructor */
+Muehle::Muehle() { srand((unsigned)time(nullptr)); }
+
+/* Muehle class destructor */
+Muehle::~Muehle() {}
+
+/* Reinitialises the Muehle object. */
+void Muehle::BeginNewGame(MuehleAI* first_player_ai, MuehleAI* second_player_ai,
+                          PlayerId current_player, bool setting_phase,
+                          bool reset_field) {
+  /* Delete history */
+  move_log_current_index = 0;
+  move_log.clear();
+
+  /* Calc beginning player */
+  if (current_player == PlayerId::player_one ||
+      current_player == PlayerId::player_two) {
+    beginning_player = current_player;
+  } else {
+    beginning_player =
+        (rand() % 2) ? PlayerId::player_one : PlayerId::player_two;
+  }
+
+  /* Create new field */
+  if (reset_field) {
+    field.Reset(beginning_player);
+  }
+  field.SetSituation(field.GetField(), setting_phase, 0);
+  if (field.GetCurPlayer().id != beginning_player) {
+    field.Invert();
+  }
+
+  /* Remember initial field */
+  initial_field = field;
+  player_one_ai = first_player_ai;
+  player_two_ai = second_player_ai;
+}
+
+/* Put a stone onto the field during the setting phase. */
+bool Muehle::PutStone(FieldPos pos, PlayerId player) {
+  /* Check perameters */
+  if (player != PlayerId::player_one && player != PlayerId::player_two) {
+    return false;
+  }
+  if (pos >= FieldStruct::size) {
+    return false;
+  }
+  if (field.GetStone(pos) != PlayerId::square_is_free) {
+    return false;
+  }
+
+  /* Set stone */
+  FieldStruct::FieldArray the_field = field.GetField();
+  the_field[pos] = player;
+  field.SetSituation(the_field, field.InSettingPhase(), 0);
+
+  /* Return success */
+  return true;
+}
+
+/* This function has to be called when the setting phase has finished */
+bool Muehle::SettingPhaseHasFinished() {
+  /* Remember initial_field */
+  initial_field = field;
+  return true;
+}
+
+/* Copy the current field state into the array p_field */
+const FieldStruct::FieldArray& Muehle::GetField() const {
+  return field.GetField();
+}
+
+/* Checks if the given move would clos */
 
 }  // namespace muehle
